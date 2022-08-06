836668fd2bd2cf6fd50842bb8ccd4168cd47687c
Pronoun <pronoun.nationstates@gmail.com>
2022-08-05 23:37:38 -0700

    fix: preserve dispatch state after moving back

--- components/SheetSection.tsx
+++ components/SheetSection.tsx
@@ -1,41 +1,120 @@
 import clsx from "clsx";
-import { EventHandler, useContext, useState } from "react";
+import { EventHandler, useContext, useEffect, useState } from "react";
+import useSWR from "swr";
 import { OptionsContext } from "./OptionsContext";
 import { SheetView } from "./SheetView";
+const Handlebars = require("handlebars/dist/handlebars");
+
+async function fetcher(resource: URL): Promise<any> {
+	const response = await fetch(resource);
+
+	return response.json();
+}
+
+function useSheet(id?: string) {
+	const {
+		data: variables,
+		error: variablesError,
+		isValidating: variablesIsValidating,
+	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Variables` : null, fetcher);
+	const {
+		data: templates,
+		error: templatesError,
+		isValidating: templatesIsValidating,
+	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Templates` : null, fetcher);
+	const {
+		data: dispatches,
+		error: dispatchesError,
+		isValidating: dispatchesIsValidating,
+	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Dispatches` : null, fetcher);
+
+	if (
+		dispatches?.error ||
+		templates?.error ||
+		variables?.error ||
+		variablesError ||
+		templatesError ||
+		dispatchesError
+	) {
+		return {
+			data: null,
+			error: true,
+			isValidating:
+				dispatchesIsValidating ||
+				templatesIsValidating ||
+				variablesIsValidating,
+		};
+	}
+
+	if (dispatches && templates && variables) {
+		let hbsInput: { [key: string]: string } = {};
+
+		variables?.forEach((variable: Variable) => {
+			hbsInput[variable["Variable Name"]] = variable["Variable Value"];
+		});
+
+		templates?.forEach((template: Template) => {
+			Handlebars.registerPartial(
+				template["Template Name"],
+				template["Template Content"]
+			);
+		});
+
+		dispatches?.forEach((dispatch: Dispatch) => {
+			const dispatchTemplate = Handlebars.compile(dispatch.Text);
+			try {
+				dispatch.Text = dispatchTemplate(hbsInput);
+			} catch (error: any) {
+				dispatch.Text = `Error parsing templates: ${error.message}`;
+			}
+		});
+	}
+
+	return {
+		data: dispatches,
+		error: dispatchesError,
+		isValidating:
+			dispatchesIsValidating || templatesIsValidating || variablesIsValidating,
+	};
+}
 
 export function SheetSection({
 	continueHandler,
 }: {
 	continueHandler?: EventHandler<any>;
 }) {
+	// const [url, setUrl] = useState("");
 	const [id, setId] = useState("");
-	const { url, setUrl, dispatches } = useContext(OptionsContext);
+	const { url, setUrl, dispatches, setDispatches } = useContext(OptionsContext);
+	const { data, isValidating, error } = useSheet(id);
 
-	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		setId(url.match(/(?<=d\/).*?(?=\/|$)/g)![0]);
-	};
+	useEffect(() => {
+		setDispatches(data);
+	}, [data]);
+
+	useEffect(() => {
+		setId(url.match(/(?<=d\/).*?(?=\/|$)/g)?.[0] ?? "");
+	}, [url]);
 
 	return (
 		<div className="md:col-span-2 md:mt-0 dark:bg-slate-800 flex flex-col mt-8 overflow-hidden bg-white rounded-md shadow">
 			<div className="flex flex-col flex-1 h-full gap-4 p-4 overflow-scroll">
-				<form className="flex-0 flex flex-row gap-4" onSubmit={handleSubmit}>
-					<input
-						type="url"
-						className="input input-bordered flex-1 !text-base"
-						placeholder="Spreadsheet URL"
-						required
-						value={url}
-						onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-							setUrl(e.currentTarget.value);
-						}}
-					/>
-					<button type="submit" className="btn flex-0 !text-base">
-						Submit
-					</button>
-				</form>
+				<input
+					type="url"
+					className="input input-bordered !text-base"
+					placeholder="Spreadsheet URL"
+					required
+					value={url}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setUrl(e.currentTarget.value);
+					}}
+				/>
 				<div className="bg-slate-50 dark:bg-slate-900/50 sm:p-8 flex-1 w-full p-4 overflow-y-auto rounded-lg shadow-inner">
-					<SheetView id={id} />
+					<SheetView
+						dispatches={data}
+						error={error}
+						isValidating={isValidating}
+					/>
 				</div>
 			</div>
 			<div className="bg-slate-50 dark:bg-slate-900/50 flex flex-row justify-end p-4">
--- components/SheetView.tsx
+++ components/SheetView.tsx
@@ -5,80 +5,18 @@ import { Disclosure } from "@headlessui/react";
 const Handlebars = require("handlebars/dist/handlebars");
 import { OptionsContext } from "./OptionsContext";
 
-async function fetcher(resource: URL): Promise<any> {
-	const response = await fetch(resource);
-	return response.json();
-}
+export function SheetView({
+	dispatches,
+	error,
+	isValidating,
+}: {
+	dispatches?: Dispatch[];
+	error?: string;
+	isValidating: boolean;
+}) {
+	if (error) return <div>Failed to load.</div>;
 
-function useSheet(id?: string) {
-	const {
-		data: variables,
-		error: variablesError,
-		isValidating: variablesIsValidating,
-	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Variables` : null, fetcher);
-	const {
-		data: templates,
-		error: templatesError,
-		isValidating: templatesIsValidating,
-	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Templates` : null, fetcher);
-	const {
-		data: dispatches,
-		error: dispatchesError,
-		isValidating: dispatchesIsValidating,
-	} = useSWR(id ? `https://opensheet.elk.sh/${id}/Dispatches` : null, fetcher);
-
-	if (dispatches?.error || templates?.error || variables?.error) {
-		return {
-			data: dispatches,
-			error: true,
-			isValidating:
-				dispatchesIsValidating ||
-				templatesIsValidating ||
-				variablesIsValidating,
-		};
-	}
-
-	if (dispatches && templates && variables) {
-		let hbsInput: { [key: string]: string } = {};
-
-		variables?.forEach((variable: Variable) => {
-			hbsInput[variable["Variable Name"]] = variable["Variable Value"];
-		});
-
-		templates?.forEach((template: Template) => {
-			Handlebars.registerPartial(
-				template["Template Name"],
-				template["Template Content"]
-			);
-		});
-
-		dispatches?.forEach((dispatch: Dispatch) => {
-			const dispatchTemplate = Handlebars.compile(dispatch.Text);
-			try {
-				dispatch.Text = dispatchTemplate(hbsInput);
-			} catch (error: any) {
-				dispatch.Text = `Error parsing templates: ${error.message}`;
-			}
-		});
-	}
-
-	return {
-		data: dispatches,
-		error: dispatchesError || templatesError || variablesError,
-		isValidating:
-			dispatchesIsValidating || templatesIsValidating || variablesIsValidating,
-	};
-}
-
-export function SheetView({ id }: { id?: string }) {
-	const { data, isValidating, error } = useSheet(id);
-	const { setDispatches } = useContext(OptionsContext);
-
-	useEffect(() => {
-		setDispatches(data);
-	}, [data]);
-
-	if (!id) {
+	if (!dispatches || !dispatches.length) {
 		return (
 			<div className="dark:text-slate-100 space-y-4">
 				<p>No dispatches loaded. Enter a spreadsheet URL to get started.</p>
@@ -98,60 +36,48 @@ export function SheetView({ id }: { id?: string }) {
 		);
 	}
 
-	if (error) return <div>Failed to load.</div>;
-
 	if (isValidating) return <div>Loading…</div>;
 
 	return (
 		<div className="space-y-4">
-			{data?.map(
-				(
-					element: {
-						Title: string;
-						Category: string;
-						Subcategory: string;
-						Text: string;
-					},
-					index: Key
-				) => {
-					return (
-						<div
-							key={index}
-							className="bg-base-100 border-slate-200 dark:border-slate-600 p-4 border rounded-md shadow-sm"
-						>
-							<h3 className="text-xl font-bold">{element.Title}</h3>
-							<p className="text-slate-500 font-medium">
-								{element.Category} → {element.Subcategory}
-							</p>
-							<Disclosure>
-								{({ open }) => (
-									<>
+			{dispatches?.map((element: Dispatch, index: Key) => {
+				return (
+					<div
+						key={index}
+						className="bg-base-100 border-slate-200 dark:border-slate-600 p-4 border rounded-md shadow-sm"
+					>
+						<h3 className="text-xl font-bold">{element.Title}</h3>
+						<p className="text-slate-500 font-medium">
+							{element.Category} → {element.Subcategory}
+						</p>
+						<Disclosure>
+							{({ open }) => (
+								<>
+									<Disclosure.Button className="text-slate-500 flex flex-row mt-2">
+										{open ? "Collapse" : "Show content"}{" "}
+										<ChevronDown
+											className={`${
+												open ? "rotate-180 transform" : ""
+											} h-6 w-6 transition duration-150 ease-in-out`}
+										/>
+									</Disclosure.Button>
+									<Disclosure.Panel className="bg-slate-50 border-slate-200 dark:border-slate-600 dark:bg-slate-700 p-2 mt-2 whitespace-pre-wrap border rounded">
+										<p>{element.Text}</p>
 										<Disclosure.Button className="text-slate-500 flex flex-row mt-2">
-											{open ? "Collapse" : "Show content"}{" "}
+											Collapse{" "}
 											<ChevronDown
 												className={`${
 													open ? "rotate-180 transform" : ""
 												} h-6 w-6 transition duration-150 ease-in-out`}
 											/>
 										</Disclosure.Button>
-										<Disclosure.Panel className="bg-slate-50 border-slate-200 dark:border-slate-600 dark:bg-slate-700 p-2 mt-2 whitespace-pre-wrap border rounded">
-											<p>{element.Text}</p>
-											<Disclosure.Button className="text-slate-500 flex flex-row mt-2">
-												Collapse{" "}
-												<ChevronDown
-													className={`${
-														open ? "rotate-180 transform" : ""
-													} h-6 w-6 transition duration-150 ease-in-out`}
-												/>
-											</Disclosure.Button>
-										</Disclosure.Panel>
-									</>
-								)}
-							</Disclosure>
-						</div>
-					);
-				}
-			)}
+									</Disclosure.Panel>
+								</>
+							)}
+						</Disclosure>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
